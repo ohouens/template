b46b11da9b30e983fc0a016585005b94e3dae581
ohouens <rohouens@yahoo.fr>
2019-07-04 01:13:59 +0200

    Grid optimization V5 + forum desc longer

--- widget/thread/script/displayThread.js
+++ widget/thread/script/displayThread.js
@@ -2,7 +2,8 @@ $(function(){
     if($("#threadGrid").length){
         $.get('index.php?origin=displayThread&getObject=firstPage&withData=Post', function(data){
             $("#threadGrid").html(data);
-            initMasonry();
+            initAction();
+            setTimeout(function() { initMasonry(); }, 500);
         });
     }
 
@@ -11,6 +12,11 @@ $(function(){
             columWidh: '.thread',
             itemSelector: '.thread',
             gutter: 5
+        });
+    }
+    function initAction(){
+        $("#threadGrid .thread").click(function(){
+            document.location.href = 'index.php?thread='+$(this).attr('num');
         });
     }
 });
