bc9f7426023665d19a3376434e321407ad1fa480
ohouens <rohouens@yahoo.fr>
2019-07-14 11:18:56 +0200

    new server to host qrCode

--- widget/qrCode/QrCode.php
+++ widget/qrCode/QrCode.php
@@ -3,7 +3,7 @@ class QrCode{
     public static function code($link, $writing){
         return '
         <div class="presentation">
-            <img id="qrCode" src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&amp;data=http://localhost/ohouens/project/onisowo/'.$link.'" alt="qrCode"/>
+            <img id="qrCode" src="https://chart.googleapis.com/chart?cht=qr&amp;chs=500x500&amp;chl=http://localhost/ohouens/project/onisowo/'.$link.'" alt="http://localhost/ohouens/project/onisowo/'.$link.'"/>
             <div class="square">
                 <div class="center">
                     '.$writing.'
