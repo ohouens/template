40a2a72d9505ae82d7c7ee15bb6010cebd340b04
ohouens <rohouens@yahoo.fr>
2019-07-28 11:37:08 +0200

    Adding of non default success message

--- widget/admin/DoneWidget.php
+++ widget/admin/DoneWidget.php
@@ -1,12 +1,12 @@
 <?php
 class DoneWidget extends Widget{
-    public function __construct($succeed){
+    public function __construct($succeed, $message="you can check your email"){
         parent::__construct(
             '',
             '<div id="operation" class="grand square">
                 <div class="center">
                     <p>
-                        '.$this->subConstruct($succeed).'
+                        '.$this->subConstruct($succeed, $message).'
                     </p>
                 </div>
             </div>',
@@ -19,11 +19,11 @@ class DoneWidget extends Widget{
         $this->build();
     }
 
-    private function subConstruct($succeed){
+    private function subConstruct($succeed, $message){
         if($succeed == 0)
             return
                 '<img src="style/icon/check.png" alt="operation succeed"><br/>
-                (you can check your email)';
+                ('.$message.')';
         return
             '<img src="style/icon/nonCheck.png" alt="operation succeed"><br/>
             (you might retry later..)';
