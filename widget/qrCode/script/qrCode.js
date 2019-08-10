0305071ab5263af1d9fbc15da18ef59b47122110
ohouens <rohouens@yahoo.fr>
2019-08-10 12:10:51 +0200

    mispelling fixed

--- widget/qrCode/script/qrCode.js
+++ widget/qrCode/script/qrCode.js
@@ -2,7 +2,7 @@ $(function(){
     $.getScript('script/utils.js');
     $("#qrCode").click(function(){
         copyToClipboard("#qrLink");
-        $("#qrInfo").text("link have been copied!");
+        $("#qrInfo").text("link has been copied!");
         setTimeout(function(){
             $("#qrInfo").text("");
         },3000);
