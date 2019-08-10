e604038cfe1a4a9fdef912671a0218dcb0ba7319
ohouens <rohouens@yahoo.fr>
2019-08-10 11:04:40 +0200

    target blank

--- widget/footer/FooterBannerWidget.php
+++ widget/footer/FooterBannerWidget.php
@@ -20,8 +20,8 @@ class FooterBannerWidget extends Widget{
                 <img id="footerLogo" src="style/logo(1).png" alt="logo onisowo">
                 <strong>ONISOWO</strong>
                 <a href="mailto:ryan@ohouens.com"><img src="style/icon/mail.png" alt="mailto"></a>
-                <a href="https://ohouens.com/"><img src="https://ohouens.com/style/icon/ohouens_blanc.png" alt="ohouens"></a>
-                <a href="https://twitter.com/otgfeak/"><img src="style/icon/twitterWhite.png" alt="twitter"></a>
+                <a href="https://ohouens.com/" target="_blank"><img src="https://ohouens.com/style/icon/ohouens_blanc.png" alt="ohouens"></a>
+                <a href="https://twitter.com/otgfeak/" target="_blank"><img src="style/icon/twitterWhite.png" alt="twitter"></a>
             </div>
         </div>';
     }
