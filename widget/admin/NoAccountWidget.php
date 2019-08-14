e3f68cdc2315946e2f59e53b18b88d551e587343
ohouens <rohouens@yahoo.fr>
2019-08-14 17:31:03 +0200

    hash mechanism

--- widget/admin/NoAccountWidget.php
+++ widget/admin/NoAccountWidget.php
@@ -1,13 +1,14 @@
 <?php
 class NoAccountWidget extends Widget{
     public function __construct(Post $post){
+        global $hash;
         parent::__construct(
             '',
             '<div class="grand square">
                 <div class="center">
                     <form method="get" id="nonMail" action="index.php">
                         <p>Please enter your email</p>
-                        <input type="hidden" name="thread" value="'.$post->getId().'">
+                        <input type="hidden" name="thread" value="'.$hash->get($post->getId()).'">
                         <input type="hidden" name="request" value="3">
                         <input type="email" name="email" class="input" placeholder="email"/><br/>
                         <span>By clicking the send button, you accept my <a href="https://onisowo.com/policy/privacy/#nonUser">privacy policy</a></span><br/>
