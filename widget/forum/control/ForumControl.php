c9bbd52c81db0c707cbc7c37f08a5f6ae5120228
ohouens <rohouens@yahoo.fr>
2019-12-27 17:48:52 +0100

    create order error fixed

--- widget/forum/control/ForumControl.php
+++ widget/forum/control/ForumControl.php
@@ -246,8 +246,8 @@ class ForumControl{
                 if(!preg_match("#^.{1,1000}$#s", $var['answer']))
                     return Constant::ERROR_CODE_THREAD_LENGTH;
                 $post->setField($var['answer']);
-                if(self::isOrder($var["answer"]) and isset($temoin->getData()["tunnel"])){
-                    if(!ThreadControl::isOpen($temoin))
+                if(self::isOrder($var["answer"])){
+                    if($post->getData()['execute'] == 1 or !isset($temoin->getData()["tunnel"]))
                         return 0;
                     $result = self::createOrder($user, $var["answer"], $temoin, $pm, $um);
                     if($result == 44)
