81462db862b5a948ce8fd6854e1916dddad5ec83
ohouens <rohouens@yahoo.fr>
2020-04-05 13:41:40 +0200

    ThreadList redirect error fixed

--- widget/thread/DisplayThreadWidget.php
+++ widget/thread/DisplayThreadWidget.php
@@ -237,7 +237,7 @@ class DisplayThreadWidget extends Widget{
         return
         '<div id="contentCode" class="grand children rectangle">
             <div class="center">
-                '.QrCode::code('index.php?thread='.$hash->get($post->getId()).'%26request=3&redirect', 'Follow').'
+                '.QrCode::code('index.php?thread='.$hash->get($post->getId()).'%26request=3%26redirect', 'Follow').'
             </div>
         </div><!--
         --><div id="displayList" class="mainSection grand children alignement">
