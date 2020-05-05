a2d7509646524ee6860f0968eb4b96892f8681ad
ohouens <rohouens@yahoo.fr>
2020-05-05 10:24:36 +0200

    New design for licence buying

--- widget/licence/LicenceWidget.php
+++ widget/licence/LicenceWidget.php
@@ -36,39 +36,22 @@ class LicenceWidget extends Widget{
         return
         '<div id="licence" style="background-image: url(\'media/fond/store/licence.jpg\');">
             <div class="super">
-                <h1>Licence</h1>
-                <p id="intro" class="alignement">
-                    <img src="media/paypal/paypal-logo.png" alt="PayPal Secure Mark">
-                </p><p id="price" class="alignement"><strong>50€</strong></p>
-                <div class="square" id="desc">
-                    <div class="center">
-                        <div class="alignement">
-                            <h2>Flux <img src="style/icon/flux.png" alt="flux_icon"></h2>
-                            <p>
-                                Mail system by qr code subscribption to fastly enable your
-                                followers about your updates, reductions, meetings..
-                            </p>
-                        </div><!--
-                        --><div class="alignement">
-                            <h2>Ticketing <img src="style/icon/ticket.png" alt="flux_icon"></h2>
-                            <p>
-                                Dematerialize ticketing system by qr code to fastly organize
-                                event for your followers.
-                            </p>
-                        </div><!--
-                        --><div class="alignement">
-                            <h2>Forum <img src="style/icon/forum.png" alt="flux_icon"></h2>
-                            <p>
-                                Forum system with exclusive actions allowing your followers
-                                to exchange with you from an organized way
-                            </p>
-                        </div>
+                <div class="center">
+                    <img id="example" src="media/licence/onisowoLicence.png" alt="licence example">
+                    <div id="desc" class="alignement">
+                        <h1>Onisowo Licence</h1>
+                        <p>
+                            Support our team and unlocked 77 thread slots.
+                            You will be able to make your own distributed network
+                            and making it whatever you want.
+                        </p>
+                        <button id="logPaypal" class="space">
+                            <img src="style/button/paypalCheckout(1).svg" alt="buy now"/>
+                            <span>Checkout</span>
+                        </button><br>
+                        <p id="price" class="alignement"><strong>50.00 €</strong></p>
                     </div>
                 </div>
-                <button id="logPaypal" class="space">
-                    <img src="style/button/paypalCheckout(1).svg" alt="buy now"/>
-                    <span>Checkout</span>
-                </button>
             </div>
         </div>';
     }
