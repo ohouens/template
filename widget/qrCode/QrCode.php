a5011c329423b3929cda3f9bdb1719a58993957a
ohouens <rohouens@yahoo.fr>
2020-06-21 10:56:31 +0200

    code improvement

--- widget/qrCode/QrCode.php
+++ widget/qrCode/QrCode.php
@@ -3,14 +3,14 @@ class QrCode{
     public static function code($link, $writing, $dimension=500){
         return '
         <div class="presentation" style="text-align: center;">
-            <img id="qrCode" src="https://chart.googleapis.com/chart?cht=qr&amp;chs='.$dimension.'x'.$dimension.'&amp;chl=http://onisowo.com/'.$link.'" alt="http://onisowo.com/'.$link.'" value="a"/>
-            <span id="qrLink" style="display:none;">http://onisowo.com/'.preg_replace("#%26#", "&", $link).'</span>
+            <img class="qrCode" src="https://chart.googleapis.com/chart?cht=qr&amp;chs='.$dimension.'x'.$dimension.'&amp;chl=http://onisowo.com/'.$link.'" alt="http://onisowo.com/'.$link.'" value="a"/>
+            <span class="qrLink" style="display:none;">http://onisowo.com/'.preg_replace("#%26#", "&", $link).'</span>
             <div class="square">
                 <div class="center">
                     '.$writing.'
                 </div>
             </div>
-        </div>
-        <div id="qrInfo" class="space"></div>';
+            <div class="qrInfo" class="space"></div>
+        </div>';
     }
 }
--- widget/qrCode/script/qrCode.js
+++ widget/qrCode/script/qrCode.js
@@ -1,14 +1,15 @@
 $(function(){
     $.getScript('script/utils.js');
-    $("#qrCode").click(function(){
-        copyToClipboard("#qrLink");
-        $("#qrInfo").text("link has been copied!");
+    $(".qrCode").click(function(){
+        copyToClipboard(".qrLink");
+        $(this).parent().find(".qrInfo").text("link has been copied!");
+        var temp = $(this).parent().find(".qrInfo");
         setTimeout(function(){
-            $("#qrInfo").text("");
+            temp.text("");
         },3000);
     });
 
-    $("#qrCode").dblclick(function(){
-        window.location.href = $(this).attr("src");
+    $(".qrCode").dblclick(function(){
+        window.location.href = $(this).parent().find(".qrLink").text();
     });
 });
