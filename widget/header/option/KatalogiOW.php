74c21601ff894fed252c68d38207ffe0de4e0e66
ohouens <rohouens@yahoo.fr>
2020-11-02 09:28:41 +0100

    adding of ios katalogi link

--- widget/header/option/KatalogiOW.php
+++ widget/header/option/KatalogiOW.php
@@ -14,7 +14,7 @@ class KatalogiOW extends OptionWidget{
     public function screen(Manager $manager){
         return '
         <span class="gold"style="vertical-align:middle;"">Check the app !</span>
-        <a href="https://play.google.com/store/apps/details?id=com.ohouens.catalogue"> <img src="style/icon/playstore.png" alt="playstore" style="height: 30px; vertical-align:middle;"></a>
-        <img src="style/icon/ios.png" alt="ios" style="height: 30px; vertical-align:middle;">';
+        <a href="https://play.google.com/store/apps/details?id=com.ohouens.catalogue" target="_blank"><img src="style/icon/playstore.png" alt="playstore" style="height: 30px; vertical-align:middle;"></a>
+        <a href="https://apps.apple.com/us/app/katalogi/id1536317968" target="_blank"><img src="style/icon/ios.png" alt="ios" style="height: 30px; vertical-align:middle;"></a>';
     }
 }
